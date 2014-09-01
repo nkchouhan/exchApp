5808cdc98281f2cc57a36fd5f68a155b44b4779b
Nandkishore <nandkishore.rails@gmail.com>
2014-09-01 22:24:00 +0530

    setup sending email from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ ExchApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.default_url_options = { host: '104.131.254.188', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    #tls: true,
+    domain: '104.131.254.188:3000',
+    authentication: "plain",
+    #enable_starttls_auto: false,
+    user_name: "evoting505@gmail.com",
+    password:"evote123"
+  }
 end
